b32457d50d5ef8b03fdd04d3b5f248b5a08f76a9
Gabriel Pereira <gabrielfpereira1@gmail.com>
2024-07-25 21:36:41 -0300

    Update adiciona-usuario-no-grupolocal.ps1

---
+++ powershell-scripts/windows-remote-administration/adiciona-usuario-no-grupolocal.ps1
@@ -0,0 +1,8 @@
+#Add-LocalGroupMember
+#   [-Group] <LocalGroup>
+#   [-Member] <LocalPrincipal[]>
+#   [-WhatIf]
+#   [-Confirm]
+#   [<CommonParameters>]
+
+Add-LocalGroupMember -Group "Administrators" -Member "Admin02", "AzureAD\DavidChew@contoso.com"
