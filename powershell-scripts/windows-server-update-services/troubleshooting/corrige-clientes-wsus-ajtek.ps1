ae5afde01c167fbb539e429a0dff200b982352be
Gabriel Pereira <gabrielfpereira1@gmail.com>
2024-08-23 12:13:15 -0300

    new-scripts-added

---
+++ powershell-scripts/windows-server-update-services/troubleshooting/corrige-clientes-wsus-ajtek.ps1
@@ -0,0 +1,8 @@
+#https://www.ajtek.ca/wsus/client-machines-not-reporting-to-wsus-properly/
+
+Stop-Service -Name BITS, wuauserv -Force
+Remove-ItemProperty -Name AccountDomainSid, PingID, SusClientId, SusClientIDValidation -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\ -ErrorAction SilentlyContinue
+Remove-Item "$env:SystemRoot\SoftwareDistribution\" -Recurse -Force -ErrorAction SilentlyContinue
+Start-Service -Name BITS, wuauserv
+wuauclt /resetauthorization /detectnow
+(New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
